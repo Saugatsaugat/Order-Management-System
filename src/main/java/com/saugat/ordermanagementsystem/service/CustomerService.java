44e0cd6bcf57aadef9e311e3f7c444bcdfbff940
Saugat Thapa <saugatthapa2073@gmail.com>
2025-07-19 17:35:04 -0500

    Feature | override customer creation method in customer service method for better handling the mechanism.

--- src/main/java/com/saugat/ordermanagementsystem/service/CustomerService.java
+++ src/main/java/com/saugat/ordermanagementsystem/service/CustomerService.java
@@ -1,13 +1,17 @@
 package com.saugat.ordermanagementsystem.service;
 
+import com.saugat.ordermanagementsystem.exceptions.CustomException;
 import com.saugat.ordermanagementsystem.mapper.CustomerMapper;
 import com.saugat.ordermanagementsystem.mapper.IBaseMapper;
 import com.saugat.ordermanagementsystem.model.Customer;
 import com.saugat.ordermanagementsystem.repo.AbstractRepo;
 import com.saugat.ordermanagementsystem.repo.CustomerRepo;
 import com.saugat.ordermanagementsystem.wrapper.CustomerVo;
+import com.saugat.ordermanagementsystem.wrapper.api.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class CustomerService extends AbstractService<Customer, CustomerVo> {
@@ -30,5 +34,15 @@ public class CustomerService extends AbstractService<Customer, CustomerVo> {
     @Override
     public IBaseMapper<Customer, CustomerVo> getMapper() {
         return mapper;
+    }
+
+    @Override
+    public ResponseEntity<ApiResponse<CustomerVo>> create(@RequestBody CustomerVo vo){
+        if(vo.getAddress() == null || vo.getAddress().getId() == null){
+            throw new CustomException("Missing values for address");
+        }
+
+        Customer c = getRepository().save(getMapper().fromDto(vo));
+        return responseBuilder(true, "Customer Updated Successfully", getMapper().toDto(c));
     }
 }
