da1c18e1717e10374f9758b91145c2456c0445ad
Saugat Thapa <saugatthapa2073@gmail.com>
2025-07-18 22:37:07 -0500

    Refactor | Added Abstract response using Spring Response Entity for better Json object representation.

--- src/main/java/com/saugat/ordermanagementsystem/resource/AbstractResponse.java
+++ src/main/java/com/saugat/ordermanagementsystem/resource/AbstractResponse.java
@@ -1,35 +1,11 @@
 package com.saugat.ordermanagementsystem.resource;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.saugat.ordermanagementsystem.wrapper.api.ApiResponse;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Collection;
-
 public abstract class AbstractResponse {
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
-    public ResponseEntity<String> responseBuilder(boolean success, String message, Object object) {
-        try {
-            JSONObject jsonObject = new JSONObject()
-                    .put("success", success)
-                    .put("message", message);
-
-            if (object instanceof JSONObject) {
-                jsonObject.put("result", object);
-            } else if (object instanceof Collection) {
-                Collection<?> col = (Collection<?>) object;
-                jsonObject.put("result", new JSONArray(objectMapper.writeValueAsString(col)));
-            } else if (object != null) {
-                jsonObject.put("result", new JSONObject(objectMapper.writeValueAsString(object)));
-            } else {
-                jsonObject.put("result", new JSONObject());
-            }
-            return ResponseEntity.ok(jsonObject.toString());
-
-        } catch (Exception ex) {
-            return ResponseEntity.internalServerError().build();
-        }
+    public <T> ResponseEntity<ApiResponse<T>> responseBuilder(boolean success, String message, T data){
+        ApiResponse<T> response = new ApiResponse<>(success, message, data);
+        return ResponseEntity.ok(response);
     }
 }
