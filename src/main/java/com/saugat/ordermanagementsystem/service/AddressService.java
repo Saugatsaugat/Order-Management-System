a63992d140734b98e462f87a7bb634ad9b6b62b5
Saugat Thapa <saugatthapa2073@gmail.com>
2025-07-23 18:20:43 -0500

    Feature | added log4j and tested the first console output

--- src/main/java/com/saugat/ordermanagementsystem/service/AddressService.java
+++ src/main/java/com/saugat/ordermanagementsystem/service/AddressService.java
@@ -6,9 +6,15 @@ import com.saugat.ordermanagementsystem.model.Address;
 import com.saugat.ordermanagementsystem.repo.AbstractRepo;
 import com.saugat.ordermanagementsystem.repo.AddressRepo;
 import com.saugat.ordermanagementsystem.wrapper.AddressVo;
+import com.saugat.ordermanagementsystem.wrapper.api.ApiResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 @Service
 @Transactional
@@ -33,5 +39,14 @@ public class AddressService extends AbstractService<Address, AddressVo> {
     @Override
     public IBaseMapper<Address, AddressVo> getMapper() {
         return mapper;
+    }
+
+    public static final Logger logger = LogManager.getLogger(AddressService.class);
+
+    @Override
+    public ResponseEntity<ApiResponse<AddressVo>> create(AddressVo vo){
+        Address address = repo.save(mapper.fromDto(vo));
+        logger.info("Address created | ID={} | timestamp={}", address.getId(), new Date());
+        return responseBuilder(true, "Address created successfully", mapper.toDto(address) );
     }
 }
