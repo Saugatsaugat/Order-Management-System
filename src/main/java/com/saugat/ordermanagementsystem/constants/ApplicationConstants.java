5bfe9b13bb80f0122de06df4cca29792de99e86b
Saugat Thapa <saugatthapa2073@gmail.com>
2025-08-27 23:16:14 -0500

    Feature | Added JWT Token generator and validator filter.
    Added dependencies in pom as well

---
+++ src/main/java/com/saugat/ordermanagementsystem/constants/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package com.saugat.ordermanagementsystem.constants;
+
+public final class ApplicationConstants {
+    public static final String JWT_SECRET_KEY = "JWT_SECRET";
+    public static final String JWT_SECRET_DEFAULT_VALUE = "f4c3b92a0df186e7c4b57905d3a81a9f3e84b1dcd63528cfd8e20ba7546f93a1";
+    public static final String JWT_HEADER = "Authorization";
+
+}
